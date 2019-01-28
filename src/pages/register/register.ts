1d5da5f00d7284d934dc7c64976ddcb012d2de55
mohitTandon <mohit.tandon5@gmail.com>
2019-01-28 18:32:52 +0530

    working on bugs

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -102,7 +102,7 @@ export class RegisterPage {
         daenerys = 0;
       }
       let query = `INSERT INTO User (CareProviderName,PhysicianName,Mobile,UserName,Password,Location,DeviceId,CreatedTime,UpdatedTime,Daenerys) VALUES  ('${formData.value['careProviderName']}','${formData.value['physicinName']}',${formData.value['mobile']},'${formData.value['userName']}','${formData.value['password']}','${formData.value['location']}','${formData.value['deviceId']}','${date}','${date}',${daenerys})`
-     
+      console.log("---query",query)
       this._db.executeQuery(query).then((res) => {
         formData.value['UserId'] = res['insertId'];
         const userData = {
@@ -133,4 +133,9 @@ export class RegisterPage {
       })
     }
   }
+  testTable(){
+    this._db.executeQuery('PRAGMA table_info(User)').then((res) => {
+    console.log("--test response:",res)
+  })
+}
 }
